31f2b92c29ab40a0f0352cc4d53887af4d0e655d
BIGBen99 <bvef@hotmail.com>
2023-09-28 07:21:39 +0200

    load config

--- log.py
+++ log.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 INFO = 'INFO '
 DEBUG = 'DEBUG'
+ERROR = 'ERROR'
 
 def log(level, message):
     print(datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") + '|' + level + '|' + message)
